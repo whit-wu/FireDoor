1065aa89aad3debf8637a318130838998b8d77cb
whit-wu <whit.wu@gmail.com>
2020-01-26 22:36:57 -0500

    Add Newtonsoft.Json.  Create TestApp model.  Add to app.json.
    Newtonsoft.Json has been added to the project to help aid in
    converting the JSON file's contents into objects.  The TestApp
    model is what the JSON objects will be converted to. Another game
    (Doom) was added to our list.

---
+++ FireDoor/Models/TestApp.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FireDoor.Models
+{
+    public class TestApp
+    {
+        public string AppName { get; set; }
+        public string ProcessName { get; set; }
+    }
+}
