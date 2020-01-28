c0e9a81e9a3850576d6513d2a6f8218b0c2b5c1b
whit-wu <whit.wu@gmail.com>
2020-01-27 19:35:39 -0500

    Migrate funtionality to seperate classes
    UpdateVisitor was migrated to its own class file of the same name,
    and the code to measure CPU temp and kill a process was moved
    to CpuTempService.

--- FireDoor/Program.cs
+++ FireDoor/Program.cs
@@ -4,81 +4,18 @@ using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using OpenHardwareMonitor.Hardware;
+using FireDoor.Services;
+
 
 namespace FireDoor { 
     class Program
     {
-        public class UpdateVisitor : IVisitor
-        {
-            public void VisitComputer(IComputer computer)
-            {
-                computer.Traverse(this);
-            }
-            public void VisitHardware(IHardware hardware)
-            {
-                hardware.Update();
-                foreach (IHardware subHardware in hardware.SubHardware) subHardware.Accept(this);
-            }
-            public void VisitSensor(ISensor sensor) { }
-            public void VisitParameter(IParameter parameter) { }
-        }
 
-        public static bool IsCPUTooHot()
-        {
-            UpdateVisitor updateVisitor = new UpdateVisitor();
-            Computer computer = new Computer();
-            computer.Open();
-            computer.CPUEnabled = true;
-            computer.Accept(updateVisitor);
-            for (int i = 0; i < computer.Hardware.Length; i++)
-            {
-                if (computer.Hardware[i].HardwareType == HardwareType.CPU)
-                {
-                    for (int j = 0; j < computer.Hardware[i].Sensors.Length; j++)
-                    {
-                        var coreTemp = computer.Hardware[i].Sensors[j].Value;
-                        if (computer.Hardware[i].Sensors[j].SensorType == SensorType.Temperature && computer.Hardware[i].Sensors[j].Name.ToLower().Contains("core"))
-                        {
-                            Console.WriteLine(computer.Hardware[i].Sensors[j].Name + ":" + coreTemp.ToString() + "\r");
-                            if (coreTemp > 50)
-                            {
-                                Console.WriteLine($"Core temp reached {Convert.ToInt32(coreTemp)}.  Max temp allowed is 80.");
-                                return true;
-                            }
-                            Console.WriteLine();
-                            
-                        }
-                        
-                    }
-                    return false;
-                }
-            }
-            computer.Close();
-            return false;
-        }
-
-        static void KillOCTestApp()
-        {
-            Console.WriteLine("Killing process");
-            foreach (var process in Process.GetProcessesByName("witcher3"))
-            {
-                process.Kill();
-            }
-        }
 
         static void Main(string[] args)
         {
-            bool IsTooHot = IsCPUTooHot();
-
-            while (!IsTooHot)
-            {
-                IsTooHot = IsCPUTooHot();
-                System.Threading.Thread.Sleep(10000);
-                Console.Clear();
-            }
-
-            KillOCTestApp();
+            CpuTempService cpuTempService = new CpuTempService();
+            cpuTempService.MeasureTemperature();
             
         }
     }
---
+++ FireDoor/Services/CpuTempService.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FireDoor.Models;
+using OpenHardwareMonitor.Hardware;
+
+namespace FireDoor.Services
+{
+    public class CpuTempService
+    {
+        public CpuTempService()
+        {
+
+        }
+
+        public CpuTempService(TestApp testApp)
+        {
+
+        }
+
+        private TestApp _testApp;
+
+        
+        public void MeasureTemperature()
+        {
+            while (!IsCPUTooHot())
+            {
+                System.Threading.Thread.Sleep(10000);
+                Console.Clear();
+            }
+            KillOCTestApp();
+        }
+        
+        private bool IsCPUTooHot()
+        {
+            UpdateVisitor updateVisitor = new UpdateVisitor();
+            Computer computer = new Computer();
+            computer.Open();
+            computer.CPUEnabled = true;
+            computer.Accept(updateVisitor);
+            for (int i = 0; i < computer.Hardware.Length; i++)
+            {
+                if (computer.Hardware[i].HardwareType == HardwareType.CPU)
+                {
+                    for (int j = 0; j < computer.Hardware[i].Sensors.Length; j++)
+                    {
+                        var coreTemp = computer.Hardware[i].Sensors[j].Value;
+                        if (computer.Hardware[i].Sensors[j].SensorType == SensorType.Temperature && computer.Hardware[i].Sensors[j].Name.ToLower().Contains("core"))
+                        {
+                            Console.WriteLine(computer.Hardware[i].Sensors[j].Name + ":" + coreTemp.ToString() + "\r");
+                            if (coreTemp > 60)
+                            {
+                                Console.WriteLine($"Core temp reached {Convert.ToInt32(coreTemp)}.  Max temp allowed is 80.");
+                                return true;
+                            }
+                            Console.WriteLine();
+                        }
+
+                    }
+                    return false;
+                }
+            }
+            computer.Close();
+            return false;
+        }
+
+        public void KillOCTestApp()
+        {
+            Console.WriteLine("Killing process");
+            foreach (var process in Process.GetProcessesByName("chrome"))
+            {
+                process.Kill();
+            }
+            Console.WriteLine("Test complete");
+            Console.ReadLine();
+        }
+    }
+}
