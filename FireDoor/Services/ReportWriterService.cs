d2e3839ca56eef576aebd9c12b9a3d0dfd86a2dd
whit-wu <whit.wu@gmail.com>
2020-04-03 11:10:27 -0400

    Add a service that will record temps and write reports

---
+++ FireDoor/Services/ReportWriterService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FireDoor.Services
+{
+    public class ReportWriterService
+    {
+
+        // method to write data to csv
+
+        // method to read data from csv
+
+        // method to calculate average temp
+
+        // method to write final report that shows average temp
+        // and time that testapp ran before terminating
+    }
+}
