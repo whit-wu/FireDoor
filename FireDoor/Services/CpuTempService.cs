27a85b7633bb5ee4d5ef89a78dcc9a59d487aa31
whit-wu <whit.wu@gmail.com>
2020-04-10 21:28:45 -0400

    Remove unused TestApp model.

--- FireDoor/Services/CpuTempService.cs
+++ FireDoor/Services/CpuTempService.cs
@@ -4,7 +4,6 @@ using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using FireDoor.Models;
 using OpenHardwareMonitor.Hardware;
 
 namespace FireDoor.Services
